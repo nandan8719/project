b459b077db568a3b4f4cadaed64ce04670e1d46c
Nandan Bhunia <nandan.bhunia25@outlook.com>
2026-03-01 13:01:11 +0530

    second commit

--- obj/Debug/net10.0/project.AssemblyInfo.cs
+++ obj/Debug/net10.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48d3f60270ef2467ec63c4d9c7cd17b01983a28")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
